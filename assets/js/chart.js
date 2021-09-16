90dce210db6815732cd69fa539e16b8ab6b14c0f
sieun <sieun0115@ajou.ac.kr>
2021-09-16 17:54:37 +0900

    fix: 0916 PM 5:54

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,4 +1,5 @@
 const category_total = {health: 852000, eatout: 617800, mart: 1183669, shopping: 5304610, oiling: 180000};
+const monthly_details = [62000, 61400, 201500, 111500, 57000, 167500, 254330, 22800, undefined, 82300, undefined, 70600, 879300, 97000, 25200, 9000, 59000, 77410, 339000, 157580, 90000, 3780800, 387710, 70650, 43600, 96900, undefined, undefined, 68000, 124999];
 
 // 도넛 차트
 new Chart(document.getElementById("doughnut-chart"), {
@@ -19,6 +20,18 @@ new Chart(document.getElementById("doughnut-chart"), {
       cutoutPercentage: 75, // 차트 굵기 조절
     }
 });
+
+const centerText = document.createElement('span');
+centerText.className = 'center-text';
+centerText.innerHTML = `${addComma(Object.values(category_total).reduce((a,b) => a+b))}<span>원</span>`
+document.querySelector('.graph-circle').appendChild(centerText);
+
+let arr=[];
+for(let i=0; i<monthly_details.length; i++){
+  arr.push(i+1);
+}
+
+
 //바 차트
 new Chart(document.getElementById("bar-chart"), {
   type: 'bar',
@@ -26,7 +39,7 @@ new Chart(document.getElementById("bar-chart"), {
     labels: [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30],
     datasets: [{
         backgroundColor: "#38c976",
-        data: [62000, 61400, 201500, 111500, 57000, 167500, 254330, 22800, undefined, 82300, undefined, 70600, 879300, 97000, 25200, 9000, 59000, 77410, 339000, 157580, 90000, 3780800, 387710, 70650, 43600, 96900, undefined, undefined, 68000, 124999]
+        data: monthly_details
     }]
   },
   options: {
@@ -43,3 +56,8 @@ new Chart(document.getElementById("bar-chart"), {
       }
   }       
 });
+
+function addComma(num) {
+  var regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(regexp, ',');
+}
